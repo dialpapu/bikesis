2cc6042c5e35f6709637f3841778e678a5b2b59d
gem install mysql2 -- '--with-mysql-lib=C:\\MySQL\\lib --with-mysql-include=C:\MySQL\include' <manuto-58@hotmail.com>
2014-10-14 16:57:53 -0500

    Cambios Interfaz

--- project/app/controllers/application_controller.rb
+++ project/app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   before_filter :configure_devise_params, if: :devise_controller?
   def configure_devise_params
   	devise_parameter_sanitizer.for(:sign_up) do |u|
-  		u.permit(:username, :userLastName, :document ,:userType,:telephone, :email, :password, :password_confirmation)
+  		u.permit(:username, :userLastName, :document ,:status ,:userType,:telephone, :email, :password, :password_confirmation)
   	end
   end
 

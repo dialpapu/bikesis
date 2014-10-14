f4b09643ecbc9a810a3b07e1ddb72cad358b663d
Diego Parra <dparrapulido@gmail.com>
2014-10-14 18:48:51 -0500

    User actualizacion

--- project/app/controllers/sellers_controller.rb
+++ project/app/controllers/sellers_controller.rb
@@ -1,11 +1,21 @@
 class SellersController < ApplicationController
 	def index
-		@sellers = User.all
+		#@sellers = User.find(:all, :conditions => { :userType => 'Seller' })
+		@sellers = User.where(:userType => 'Seller')
 		respond_to do |format|
 			format.html
 			format.pdf do
 				generateReport(@sellers)
 			end
 		end
+	end
+	private
+	def set_seller
+		@seller = User.find(params[:id])
+	end
+
+	def seller_params
+		params.require(:seller).permit(:id,:username, :userLastName,:document,:telephone, :userType, :status)
+
 	end
 end

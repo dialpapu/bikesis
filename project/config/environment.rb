0715dbdcb7aaa51ae58dc212e425191738e057c0
Diego Parra <dparrapulido@gmail.com>
2014-10-13 22:43:44 -0500

    Reporte de accesorios creado full

--- project/config/environment.rb
+++ project/config/environment.rb
@@ -1,6 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-
+require 'pdf/writer'
+require 'pdf/writer/simple_table'
 # Initialize the Rails application.
 Rails.application.initialize!
 Mime::Type.register 'application/pdf', :pdf

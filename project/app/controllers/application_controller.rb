843bdbc29afa8263c6a1da69e930858e4b6ea6c1
gem install mysql2 -- '--with-mysql-lib=C:\\MySQL\\lib --with-mysql-include=C:\MySQL\include' <manuto-58@hotmail.com>
2014-10-13 19:58:20 -0500

    Cambios

--- project/app/controllers/application_controller.rb
+++ project/app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
+	before_filter :authenticate_user!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :configure_devise_params, if: :devise_controller?
+  def configure_devise_params
+  	devise_parameter_sanitizer.for(:sign_up) do |u|
+  		u.permit(:personId, :document, :personName,:lastName,:telephone, :personStatus,:email, :password, :password_confirmation)
+  	end
+
+
+  	def show
+  		@user = User.find(params[:id])
+  	end
+  end
 end
